a5ca6ffacb9a24a7b3c1933ccb078b82a04fbd4a
gTempesta <giorgio.tempesta@gmail.com>
2014-05-28 21:09:19 +0200

    inizializza

---
+++ app/models/Page.php
@@ -0,0 +1,14 @@
+<?php namespace App\Models;
+
+class Page extends \Eloquent {
+
+	protected $table = 'pages';
+
+	public function author()
+	{
+		return $this->belongsTo('App\Models\User', 'user_id');
+		//return $this->belongsTo('User');
+	}
+
+}
+
